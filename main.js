8317212c3cb6bb822fe6e335aa3a73e1880a194f
AbdallahLearn <abdallahjhn1@gmail.com>
2024-09-08 11:56:54 +0300

    finsh the lab1 of day1 from week 3

---
+++ main.js
@@ -0,0 +1,66 @@
+// Q1
+console.log("------------------------Q1\n")
+let width = 12;
+let height = 8;
+let result = width* height;
+console.log(result)
+
+
+// Q2
+console.log("------------------------Q2\n")
+let celsius =37;
+const covertFahrenheit = (celsius * 9 / 5) + 32
+let fahrenheit = 100;
+const convertCelsius = (fahrenheit - 32) * 5 / 9;
+
+console.log(`F is ${covertFahrenheit} and C is ${convertCelsius}`)
+
+
+// Q3
+console.log("------------------------Q3\n")
+let hours = 3;
+
+let convertTosecond = hours*60*60;
+console.log(`${hours} hours = ${convertTosecond} second`)
+
+
+// Q4
+console.log("------------------------Q4\n")
+let thingOne = "Abdulllah";
+let thingTwo = "Aljohani";
+let fullName = thingOne +" "+ thingTwo;
+console.log(fullName)
+
+
+// Q5
+console.log("------------------------Q5\n")
+var bill = 10.25 + 3.99 + 7.15;
+
+var tip = bill * 0.15;
+
+var total = bill + tip;
+
+console.log(total);
+
+
+//Q6
+console.log("------------------------Q6\n")
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+
+var madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+
+console.log(madLib);
+
+// Q7
+console.log("------------------------Q7\n")
+
+var firstName = "Julia"; 
+var interest = "cats"; 
+var hobby = "play video games"; 
+
+var awesomeMessage = "Hi, my name is " + firstName + ". I love " + interest + ". In my spare time, I like to " + hobby + ".";
+
+console.log(awesomeMessage);
+
